e048d89a9321b2f85b5f978ee91a88c52ccd5326
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-06-30 22:57:15 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,54 @@
+//!It's another hypertext format to describe GUI for native application.
+//!
+//!```
+//!use aht::markup::{Page, TypeEntity};
+//!
+//!TypeEntity::from_str(&s);
+//!
+//!Page::from_str(&s);
+//!```
+//!
+
 #![allow(dead_code)]
 
-mod element;
-mod grid;
-mod text;
+#[macro_use]
+mod macros;
+pub mod content;
+pub mod css;
+pub mod font;
+pub mod grid;
+pub mod head;
+pub mod markup;
+pub mod parts;
+pub mod script;
+mod utils;
 
+#[cfg(test)]
+mod tests {
+
+    use crate::markup::{Page, TypeEntity};
+
+    #[test]
+    fn test() {
+        let s = "<aht>
+        <head lang=en>
+            <title>1</title>
+        </head>
+        <body column=\"\" row=\"2\">
+            <inp name=\"\" value=\"\" readonly required>input</inp>
+            <button href=\"\" async=>button</button>
+            <area class=\"\" id=\"\" width=\"1000\" height=\"100\" column=2 row=\"\"></area>
+        </body>
+        <css>
+        </css>
+        <script>
+        </script>
+     </aht>";
+        if let Some(e) = TypeEntity::from_str(&s) {
+            println!("{:?}", e);
+        }
+        if let Some(e) = Page::from_str(&s) {
+            println!("{:?}", e);
+        }
+    }
+}
