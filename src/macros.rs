857f1c759f374eb065ae8154163a0c954edcb5a2
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-07-12 23:18:43 +0800

    Update macros.rs

--- src/macros.rs
+++ src/macros.rs
@@ -1,12 +1,23 @@
 macro_rules! get_set {
-    ($field:ident, $set:ident, $t:ty) => {
+    ($field:ident, $t:ty) => {
         ///Returns a reference.
         pub fn $field(&self) -> &$t {
             &self.$field
         }
+    };
+    ($field:ident, $set:ident, $t:ty) => {
+        get_set!($field, $t);
 
         pub fn $set(&mut self, s: $t) {
             self.$field = s;
+        }
+    };
+    ($field:ident, $mut:ident, $set:ident, $t:ty) => {
+        get_set!($field, $set, $t);
+
+        ///Returns a mutable reference.
+        pub fn $mut(&mut self) -> &mut $t {
+            &mut self.$field
         }
     };
 }
@@ -19,11 +30,13 @@ macro_rules! element {
     };
 }
 
-macro_rules! id_class {
+macro_rules! subset {
     () => {
-        get_set!(id, set_id, String);
+        get_set!(subset, Subset);
 
-        get_set!(class, set_class, String);
+        pub fn push_subset(&mut self, e: TypeEntity) {
+            self.subset.vec().push_back(e);
+        }
     };
 }
 
@@ -33,45 +46,31 @@ macro_rules! text {
     };
 }
 
-macro_rules! subset {
+macro_rules! action {
     () => {
-        pub fn subset(&self) -> &VecDeque<TypeEntity> {
-            &self.subset
-        }
-
-        pub fn push_subset(&mut self, e: TypeEntity) {
-            self.subset.push_back(e);
-        }
+        get_set!(action, set_action, String);
     };
 }
 
-macro_rules! tip {
+macro_rules! asynchronous {
     () => {
-        get_set!(tip, set_tip, String);
+        get_set!(asynchronous, set_asynchronous, bool);
     };
 }
 
-macro_rules! hidden {
+macro_rules! class_id {
     () => {
-        get_set!(hidden, set_hidden, bool);
+        get_set!(class, set_class, String);
+
+        get_set!(id, set_id, String);
     };
 }
 
-macro_rules! name {
+macro_rules! column_row {
     () => {
-        get_set!(name, set_name, String);
-    };
-}
+        get_set!(column, set_column, Points);
 
-macro_rules! value {
-    () => {
-        get_set!(value, set_value, String);
-    };
-}
-
-macro_rules! readonly {
-    () => {
-        get_set!(readonly, set_readonly, bool);
+        get_set!(row, set_row, Points);
     };
 }
 
@@ -81,55 +80,103 @@ macro_rules! disabled {
     };
 }
 
+macro_rules! enctype {
+    () => {
+        get_set!(enctype, set_enctype, String);
+    };
+}
+
+macro_rules! hidden {
+    () => {
+        get_set!(hidden, set_hidden, bool);
+    };
+}
+
+macro_rules! href {
+    () => {
+        get_set!(href, set_href, String);
+    };
+}
+
+macro_rules! method {
+    () => {
+        get_set!(method, set_method, String);
+    };
+}
+
+macro_rules! multiple {
+    () => {
+        get_set!(multiple, set_multiple, bool);
+    };
+}
+
+macro_rules! name {
+    () => {
+        get_set!(name, set_name, String);
+    };
+}
+
+macro_rules! ordinal {
+    () => {
+        get_set!(ordinal, set_ordinal, Ordinal);
+    };
+}
+
+macro_rules! readonly {
+    () => {
+        get_set!(readonly, set_readonly, bool);
+    };
+}
+
 macro_rules! required {
     () => {
         get_set!(required, set_required, bool);
     };
 }
 
-macro_rules! column_row {
+macro_rules! selected {
     () => {
-        get_set!(column, set_column, i32);
-
-        get_set!(row, set_row, i32);
+        get_set!(selected, set_selected, bool);
     };
 }
 
-macro_rules! xpoints_ypoints {
+macro_rules! src {
     () => {
-        get_set!(xpoints, set_xpoints, Vec<i32>);
+        get_set!(src, set_src, String);
+    };
+}
 
-        get_set!(ypoints, set_ypoints, Vec<i32>);
+macro_rules! tip {
+    () => {
+        get_set!(tip, set_tip, String);
+    };
+}
+
+macro_rules! value {
+    () => {
+        get_set!(value, set_value, String);
     };
 }
 
 macro_rules! range_background {
     () => {
         ///Set point on rectangular coordinates.
-        pub fn set_xy(&mut self, x: i32, y: i32) {
-            self.range.left = x;
-            self.range.top = y;
+        pub fn set_xy(&mut self, x: isize, y: isize) {
+            self.range.x = x;
+            self.range.y = y;
         }
 
-        ///Set width and height.
-        pub fn set_side(&mut self, width: i32, height: i32) {
-            self.range.right = self.range.left.saturating_add(width);
-            self.range.bottom = self.range.top.saturating_add(height);
+        ///Set width.
+        pub fn set_width(&mut self, width: isize) {
+            self.range.width = width;
         }
 
-        ///Returns a reference.
-        pub fn range(&self) -> &IRect {
-            &self.range
+        ///Set height.
+        pub fn set_height(&mut self, height: isize) {
+            self.range.height = height;
         }
 
-        ///Returns a mutable reference.
-        pub fn mut_range(&mut self) -> &mut IRect {
-            &mut self.range
-        }
-
-        pub(crate) fn to_rect(&self) -> Rect {
-            Rect::from_irect(self.range)
-        }
+        get_set!(range, mut_range, set_range, RectangleRange);
 
         get_set!(background, set_background, Color);
     };
@@ -143,9 +190,9 @@ macro_rules! align {
     };
 }
 
-macro_rules! font_color {
+macro_rules! apply_font {
     () => {
-        get_set!(font_color, set_font_color, Color);
+        get_set!(apply_font, mut_apply_font, set_apply_font, ApplyFont);
     };
 }
 
@@ -155,7 +202,7 @@ macro_rules! shape_background_border {
 
         get_set!(shape_background, set_shape_background, Color);
 
-        get_set!(border_width, set_border_width, i32);
+        get_set!(border_width, set_border_width, isize);
 
         get_set!(border_color, set_border_color, Color);
     };
@@ -163,49 +210,44 @@ macro_rules! shape_background_border {
 
 macro_rules! cursor_x {
     () => {
-        get_set!(cursor_x, set_cursor_x, i32);
+        get_set!(cursor_x, set_cursor_x, isize);
 
-        get_set!(cursor_width, set_cursor_width, i32);
+        get_set!(cursor_width, set_cursor_width, isize);
     };
 }
 
 macro_rules! cursor_y {
     () => {
-        get_set!(cursor_y, set_cursor_y, i32);
+        get_set!(cursor_y, set_cursor_y, isize);
 
-        get_set!(cursor_height, set_cursor_height, i32);
-    };
-}
-
-macro_rules! href {
-    () => {
-        get_set!(href, set_href, String);
-    };
-}
-
-macro_rules! multiple {
-    () => {
-        get_set!(multiple, set_multiple, bool);
-    };
-}
-
-macro_rules! selected {
-    () => {
-        get_set!(selected, set_selected, bool);
+        get_set!(cursor_height, set_cursor_height, isize);
     };
 }
 
 //
 //
 macro_rules! to_type {
-    ($o:ident, $t:ty, $s:ident, $attr:ident, $subset:ident $(, $pattern:pat_param, $rst:expr)*) => {
-        let mut $o = <$t>::new();
-        $o.set_text($s);
-        for attr in $attr.drain(..) {
-            $o.attr(attr);
-        }
-        for sub in $subset.drain(..) {
-            $o.push_subset(TypeEntity::from(sub));
+    ($t:ty, $s:ident, $attr:ident, $subset:ident) => {{
+        let mut o = <$t>::new();
+        o.set_text($s);
+        $attr.drain(..).for_each(|a| o.attr(a));
+        $subset
+            .drain(..)
+            .for_each(|sub| o.push_subset(TypeEntity::from(sub)));
+        o
+    }};
+    ($t:ty, $s:ident, $attr:ident) => {{
+        let mut o = <$t>::new();
+        o.set_text($s);
+        $attr.drain(..).for_each(|a| o.attr(a));
+        o
+    }};
+}
+
+macro_rules! ordinal_xy {
+    ($subset_xy:ident, $o:ident) => {
+        if let Some((x, y)) = $subset_xy.next($o.ordinal()) {
+            $o.set_xy(x, y);
         }
     };
 }
