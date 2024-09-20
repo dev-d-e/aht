b3b75ff94181b3297c88ca73610135ccf12f9131
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-09-20 16:03:49 +0800

    Update mod.rs

--- src/content/mod.rs
+++ src/content/mod.rs
@@ -2,17 +2,17 @@ mod form;
 mod media;
 mod other;
 
-use crate::parts::FixedRect;
+use crate::parts::{Coord2D, FixedRect};
 use crate::utils::color::*;
 pub use form::{Button, Form, Inp, Opt, Pt, Select, Time};
 pub use media::{Audio, Img, Video};
 pub use other::{Canv, Iframe};
-use skia_safe::{Canvas, Color, Paint, RRect};
+use skia_safe::{scalar, Canvas, Color, Paint, RRect};
 
 pub trait OutPainter: std::fmt::Debug {
     fn act(&mut self, rect: &FixedRect, canvas: &Canvas);
 
-    fn final_position(&self) -> (isize, isize);
+    fn final_position(&self) -> &Coord2D;
 }
 
 #[derive(Debug)]
@@ -25,13 +25,12 @@ pub(crate) struct Border {
     pub(crate) border_color: Color,
     pub(crate) x_rad: isize,
     pub(crate) y_rad: isize,
-    pub(crate) max_x: isize,
-    pub(crate) max_y: isize,
+    pub(crate) max: Coord2D,
 }
 
 impl Border {
     pub(crate) fn new() -> Self {
-        Border {
+        Self {
             bg_color: BG_COLOR,
             left: 1,
             top: 1,
@@ -40,14 +39,16 @@ impl Border {
             border_color: BORDER_COLOR,
             x_rad: 0,
             y_rad: 0,
-            max_x: 0,
-            max_y: 0,
+            max: Coord2D::new(),
         }
     }
 
     fn max(&mut self, rect: &FixedRect) {
-        self.max_x = rect.x + rect.width;
-        self.max_y = rect.y + rect.height;
+        self.max = rect.right_bottom();
+    }
+
+    pub(crate) fn to_rrect(&self, rect: &FixedRect) -> RRect {
+        RRect::new_rect_xy(rect.to_rect(), self.x_rad as scalar, self.y_rad as scalar)
     }
 }
 
@@ -58,16 +59,13 @@ impl OutPainter for Border {
         paint.set_anti_alias(true);
         let r = rect.add(self.left, self.top, self.right, self.bottom);
         self.max(&r);
-        let x_rad = self.x_rad as f32;
-        let y_rad = self.y_rad as f32;
-        let r = RRect::new_rect_xy(r.to_rect(), x_rad, y_rad);
-        canvas.draw_rrect(r, &paint);
+        canvas.draw_rrect(self.to_rrect(&r), &paint);
+
         paint.set_color(self.bg_color);
-        let r = RRect::new_rect_xy(rect.to_rect(), x_rad, y_rad);
-        canvas.draw_rrect(r, &paint);
+        canvas.draw_rrect(self.to_rrect(rect), &paint);
     }
 
-    fn final_position(&self) -> (isize, isize) {
-        (self.max_x, self.max_y)
+    fn final_position(&self) -> &Coord2D {
+        &self.max
     }
 }
