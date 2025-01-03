27908cb0967cf20b0cfa4045eb24143a2ec113d8
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-01-03 22:59:32 +0800

    Update mod.rs

--- src/style/mod.rs
+++ src/style/mod.rs
@@ -1,3 +1,25 @@
 mod css;
 
 use crate::markup::Page;
+use css::CssParser;
+
+#[derive(Debug)]
+pub(crate) struct StyleContext {
+    parser: Option<CssParser>,
+}
+
+impl StyleContext {
+    pub(crate) fn new() -> Self {
+        Self { parser: None }
+    }
+
+    pub fn build(&mut self, s: &str, page: &mut Page) {
+        if self.parser.is_none() {
+            self.parser.replace(CssParser::new());
+        }
+
+        if let Some(p) = &mut self.parser {
+            p.parse(s, page);
+        }
+    }
+}
