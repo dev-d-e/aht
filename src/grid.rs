c1f9f130d7c02fdf75b5ae4b6cf290600ce99bf3
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-01-08 21:53:28 +0800

    Update grid.rs

--- src/grid.rs
+++ src/grid.rs
@@ -2,18 +2,15 @@ use crate::content::{
     Area, Audio, Button, Canv, Form, Iframe, Img, Inp, Opt, Pt, Select, Time, Video,
 };
 use crate::global::*;
-use crate::markup::Mark;
-use crate::parts::{
-    Chronograph, Coord, Coord2D, Distance, FixedRect, LineSegment, Ordinal, Points, RectSide,
-};
+use crate::markup::{AttrName, Attribute, Element, Mark};
+use crate::parts::{Chronograph, Coord, Coord2D, FixedRect, LineSegment, Ordinal, RectSide};
 use skia_safe::utils::text_utils::Align;
-use skia_safe::{scalar, Canvas, Color, Font, Paint, RRect};
-use std::fmt::Debug;
+use skia_safe::{Canvas, Color, Font, Paint};
 use std::sync::Arc;
 
 ///Grid.
 #[derive(Debug)]
-pub struct Grid {
+pub(crate) struct Grid {
     x: Vec<LineSegment>,
     x_n: usize,
     y: Vec<LineSegment>,
@@ -30,12 +27,13 @@ impl Grid {
         }
     }
 
-    pub(crate) fn x(&mut self, p: &Points, r: &RectSide, zero: &Coord) {
-        self.x = p.coord(r.width, zero.x);
-    }
-
-    pub(crate) fn y(&mut self, p: &Points, r: &RectSide, zero: &Coord) {
-        self.y = p.coord(r.height, zero.y);
+    pub(crate) fn get_attr(&mut self, e: &Element, r: &RectSide, zero: &Coord) {
+        if let Some(Attribute::COLUMN(a)) = e.attribute.get(&AttrName::COLUMN) {
+            self.x = a.coord(r.width, zero.x);
+        }
+        if let Some(Attribute::ROW(a)) = e.attribute.get(&AttrName::ROW) {
+            self.y = a.coord(r.height, zero.y);
+        }
     }
 
     pub(crate) fn next(&mut self, ordinal: &Ordinal) -> Option<FixedRect> {
@@ -72,11 +70,6 @@ impl Grid {
         }
         None
     }
-}
-
-pub trait Painter: std::fmt::Debug {
-    fn set_color(&mut self, color: Color);
-    fn act(&mut self, rect: &FixedRect, canvas: &Canvas);
 }
 
 ///DrawUnit.
@@ -141,7 +134,7 @@ impl std::fmt::Debug for ApplyFont {
 }
 
 impl ApplyFont {
-    pub fn new() -> Self {
+    pub(crate) fn new() -> Self {
         Self {
             name: String::new(),
             color: None,
@@ -151,7 +144,7 @@ impl ApplyFont {
         }
     }
 
-    pub fn with(s: &str) -> Option<Self> {
+    pub(crate) fn with(s: &str) -> Option<Self> {
         get_applied(s).map(|font| Self {
             name: s.to_string(),
             color: None,
@@ -161,11 +154,11 @@ impl ApplyFont {
         })
     }
 
-    pub fn font(&self) -> &Font {
+    pub(crate) fn font(&self) -> &Font {
         &self.font
     }
 
-    pub fn color(&self) -> Option<&Color> {
+    pub(crate) fn color(&self) -> Option<&Color> {
         self.color.as_ref()
     }
 
@@ -219,70 +212,51 @@ impl Drop for ApplyFont {
 ///Sides.
 #[derive(Debug)]
 pub(crate) struct Sides {
-    pub width: isize,
-    pub height: isize,
-    pub(crate) effect: RectSide,
+    reserve: RectSide,
+    effect: RectSide,
 }
 
 impl Sides {
     pub(crate) fn new(width: isize, height: isize) -> Self {
         Self {
-            width,
-            height,
+            reserve: RectSide::new(width, height),
             effect: RectSide::empty(),
         }
     }
 
-    // pub(crate) fn pixel(w: isize, h: isize) -> Self {
-    //     Self::new(Distance::Pixel(w), Distance::Pixel(h))
-    // }
-
-    // pub(crate) fn percentage(w: usize, h: usize) -> Self {
-    //     Self::new(Distance::Percentage(w), Distance::Percentage(h))
-    // }
-
-    // pub(crate) fn full_horizontal(n: isize) -> Self {
-    //     Self::new(Distance::Percentage(100), Distance::Pixel(n))
-    // }
-
-    // pub(crate) fn full_vertical(n: isize) -> Self {
-    //     Self::new(Distance::Pixel(n), Distance::Percentage(100))
-    // }
-
-    // pub fn is_empty(&self) -> bool {
-    //     self.width.is_empty() || self.height.is_empty()
-    // }
-
-    pub(crate) fn value(&mut self, w: &Distance, h: &Distance) -> &RectSide {
-        self.effect.width = w.get(self.width);
-        self.effect.height = h.get(self.height);
-        &self.effect
+    pub(crate) fn reserve(&mut self, width: isize, height: isize) {
+        self.reserve.width = width;
+        self.reserve.height = height;
+        self.effect.width = width;
+        self.effect.height = height;
     }
 
-    pub(crate) fn value_with(&mut self, r: &RectSide) -> &RectSide {
-        //     self.value(r.width, r.height)
-        self.effect.width = r.width;
-        self.effect.height = r.height;
+    pub(crate) fn replace(&mut self, r: &RectSide) {
+        self.reserve.replace(r);
+        self.effect.replace(r);
+    }
+
+    pub(crate) fn get_attr(&mut self, e: &Element) {
+        if let Some(Attribute::WIDTH(a)) = e.attribute.get(&AttrName::WIDTH) {
+            self.effect.width = a.get(self.reserve.width);
+        }
+        if let Some(Attribute::HEIGHT(a)) = e.attribute.get(&AttrName::HEIGHT) {
+            self.effect.height = a.get(self.reserve.height);
+        }
+    }
+
+    pub(crate) fn effect(&self) -> &RectSide {
         &self.effect
     }
 
     pub(crate) fn to_rect(&self, zero: &Coord) -> FixedRect {
-        if self.effect.width == 0 {
-            //self.effect.width = self.width.get(0);
-        }
-        if self.effect.height == 0 {
-            //self.effect.height = self.height.get(0);
-        }
-        FixedRect {
-            pos: zero.to_2d(),
-            side: self.effect.clone(),
-        }
+        self.effect.to_rect(zero)
     }
 }
 
 ///HorizontalAlign.
 #[derive(Debug)]
-pub enum HorizontalAlign {
+pub(crate) enum HorizontalAlign {
     Left,
     Center,
     Right,
@@ -290,7 +264,7 @@ pub enum HorizontalAlign {
 
 ///VerticalAlign.
 #[derive(Debug)]
-pub enum VerticalAlign {
+pub(crate) enum VerticalAlign {
     Top,
     Middle,
     Bottom,
@@ -298,9 +272,9 @@ pub enum VerticalAlign {
 
 ///AlignPattern.
 #[derive(Debug)]
-pub struct AlignPattern {
-    pub horizontal: HorizontalAlign,
-    pub vertical: VerticalAlign,
+pub(crate) struct AlignPattern {
+    horizontal: HorizontalAlign,
+    vertical: VerticalAlign,
 }
 
 impl AlignPattern {
@@ -319,7 +293,7 @@ impl AlignPattern {
         Self::new(HorizontalAlign::Left, VerticalAlign::Middle)
     }
 
-    pub fn font_xy(&self, rect: &FixedRect, size: isize) -> (Coord2D, Align) {
+    pub(crate) fn font_xy(&self, rect: &FixedRect, size: isize) -> (Coord2D, Align) {
         let mut x = rect.pos.x;
         let mut font_align = Align::Left;
         match self.horizontal {
@@ -347,225 +321,5 @@ impl AlignPattern {
             }
         }
         (Coord2D::xy(x, y), font_align)
-    }
-}
-
-#[derive(Debug)]
-pub(crate) struct Range {
-    pub(crate) color: Color,
-    pub(crate) x_rad: isize,
-    pub(crate) y_rad: isize,
-}
-
-impl Range {
-    pub(crate) fn new() -> Self {
-        Self {
-            color: *default_bg_color(),
-            x_rad: 0,
-            y_rad: 0,
-        }
-    }
-
-    pub(crate) fn to_rrect(&self, rect: &FixedRect) -> RRect {
-        RRect::new_rect_xy(rect.to_rect(), self.x_rad as scalar, self.y_rad as scalar)
-    }
-}
-
-impl Painter for Range {
-    fn set_color(&mut self, color: Color) {
-        self.color = color;
-    }
-
-    fn act(&mut self, rect: &FixedRect, canvas: &Canvas) {
-        let mut paint = Paint::default();
-        paint.set_color(self.color);
-        paint.set_anti_alias(true);
-        canvas.draw_rrect(self.to_rrect(rect), &paint);
-    }
-}
-
-///ScrollBarType.
-#[derive(Debug)]
-pub enum ScrollBarType {
-    Both,
-    Horizontal,
-    Vertical,
-}
-
-///"ScrollBar" include horizontal scroll bar & vertical scroll bar.
-#[derive(Debug)]
-pub(crate) struct ScrollBar {
-    scroll_bar_type: ScrollBarType,
-    pub(crate) background: Box<dyn Painter>,
-    pub(crate) foreground: Box<dyn Painter>,
-    hor_show: bool,
-    hor_fr: FixedRect,
-    hor_fore: LineSegment,
-    ver_show: bool,
-    ver_fr: FixedRect,
-    ver_fore: LineSegment,
-}
-
-impl ScrollBar {
-    pub(crate) fn new() -> Self {
-        Self {
-            scroll_bar_type: ScrollBarType::Both,
-            background: Box::new(range!(Color::CYAN)),
-            foreground: Box::new(range!(Color::YELLOW)),
-            hor_show: false,
-            hor_fr: FixedRect::with_side(0, 10),
-            hor_fore: LineSegment::new(0, 0),
-            ver_show: false,
-            ver_fr: FixedRect::with_side(10, 0),
-            ver_fore: LineSegment::new(0, 0),
-        }
-    }
-
-    pub(crate) fn horizontal() -> Self {
-        let mut s = Self::new();
-        s.scroll_bar_type = ScrollBarType::Horizontal;
-        s
-    }
-
-    pub(crate) fn vertical() -> Self {
-        let mut s = Self::new();
-        s.scroll_bar_type = ScrollBarType::Vertical;
-        s
-    }
-
-    pub(crate) fn cursor_move(&mut self, point: &Coord2D, displacement: &RectSide) {
-        match self.scroll_bar_type {
-            ScrollBarType::Both => {
-                if self.hor_fr.within(point) {
-                    if displacement.width == 0 {
-                        return;
-                    }
-                    let max = self.hor_fr.side.width;
-                    self.hor_fore.finite_move(displacement.width, 0, max);
-                } else if self.ver_fr.within(point) {
-                    if displacement.height == 0 {
-                        return;
-                    }
-                    let max = self.ver_fr.side.height;
-                    self.ver_fore.finite_move(displacement.height, 0, max);
-                }
-            }
-            ScrollBarType::Horizontal => {
-                if self.hor_fr.within(point) {
-                    if displacement.width == 0 {
-                        return;
-                    }
-                    let max = self.hor_fr.side.width;
-                    self.hor_fore.finite_move(displacement.width, 0, max);
-                }
-            }
-            ScrollBarType::Vertical => {
-                if self.ver_fr.within(point) {
-                    if displacement.height == 0 {
-                        return;
-                    }
-                    let max = self.ver_fr.side.height;
-                    self.ver_fore.finite_move(displacement.height, 0, max);
-                }
-            }
-        }
-    }
-
-    pub(crate) fn resize(&mut self, r: &FixedRect, max: &RectSide) -> RectSide {
-        let vision = &r.side;
-        if vision.is_empty() {
-            return RectSide::new(0, 0);
-        }
-
-        match self.scroll_bar_type {
-            ScrollBarType::Both => {
-                if vision.height < max.height {
-                    self.ver_show = true;
-                    self.ver_fr.vertical_inset(r, r.width_sub(&self.ver_fr));
-                    self.ver_fore.length = vision.height * vision.height / max.height;
-                    let h = self.ver_fr.side.height - self.ver_fore.length;
-                    self.ver_fore.max_begin(h);
-                } else {
-                    self.ver_show = false;
-                    self.ver_fore.begin = 0;
-                }
-
-                if vision.width < max.width {
-                    self.hor_show = true;
-                    self.hor_fr.horizontal_inset(r, r.height_sub(&self.hor_fr));
-                    self.hor_fore.length = vision.width * vision.width / max.width;
-                    //deal with coincidence
-                    if self.ver_show && !self.ver_fr.is_empty() {
-                        self.hor_fr.side.width -= self.ver_fr.side.width;
-                    }
-                    let w = self.hor_fr.side.width - self.hor_fore.length;
-                    self.hor_fore.max_begin(w);
-                } else {
-                    self.hor_show = false;
-                    self.hor_fore.begin = 0;
-                }
-                RectSide::new(
-                    self.hor_fore.begin * max.width / vision.width,
-                    self.ver_fore.begin * max.height / vision.height,
-                )
-            }
-            ScrollBarType::Horizontal => {
-                if vision.width < max.width {
-                    self.hor_show = true;
-                    self.hor_fr.horizontal_inset(r, r.height_sub(&self.hor_fr));
-                    self.hor_fore.length = vision.width * vision.width / max.width;
-                    let w = self.hor_fr.side.width - self.hor_fore.length;
-                    self.hor_fore.max_begin(w);
-                } else {
-                    self.hor_show = false;
-                    self.hor_fore.begin = 0;
-                }
-                RectSide::new(self.hor_fore.begin * max.width / vision.width, 0)
-            }
-            ScrollBarType::Vertical => {
-                if vision.height < max.height {
-                    self.ver_show = true;
-                    self.ver_fr.vertical_inset(r, r.width_sub(&self.ver_fr));
-                    self.ver_fore.length = vision.height * vision.height / max.height;
-                    let h = self.ver_fr.side.height - self.ver_fore.length;
-                    self.ver_fore.max_begin(h);
-                } else {
-                    self.ver_show = false;
-                    self.ver_fore.begin = 0;
-                }
-                RectSide::new(0, self.ver_fore.begin * max.height / vision.height)
-            }
-        }
-    }
-
-    fn hor_draw(&mut self, canvas: &Canvas) {
-        if self.hor_show && !self.hor_fr.is_empty() {
-            self.background.act(&self.hor_fr, canvas);
-            let fg = self.hor_fr.horizontal_move(&self.hor_fore);
-            self.foreground.act(&fg, canvas);
-        }
-    }
-
-    fn ver_draw(&mut self, canvas: &Canvas) {
-        if self.ver_show && !self.ver_fr.is_empty() {
-            self.background.act(&self.ver_fr, canvas);
-            let fg = self.ver_fr.vertical_move(&self.ver_fore);
-            self.foreground.act(&fg, canvas);
-        }
-    }
-
-    pub(crate) fn draw(&mut self, canvas: &Canvas) {
-        match self.scroll_bar_type {
-            ScrollBarType::Both => {
-                self.ver_draw(canvas);
-                self.hor_draw(canvas);
-            }
-            ScrollBarType::Horizontal => {
-                self.hor_draw(canvas);
-            }
-            ScrollBarType::Vertical => {
-                self.ver_draw(canvas);
-            }
-        }
     }
 }
