29e221bf05420a97ca5a72d590fd82843f12f009
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-12-01 16:30:15 +0800

    Update macros.rs

--- src/macros.rs
+++ src/macros.rs
@@ -1,46 +1,94 @@
-macro_rules! element {
-    () => {
-        pub(crate) fn element(&self) -> &Arc<RwLock<Element>> {
-            &self.element
+macro_rules! attribute_get {
+    ($n:ident, $t:ty, $a:tt) => {
+        pub(crate) fn $n(&self) -> Option<&$t> {
+            if let Some(Attribute::$a(o)) = self.0.get(&AttrName::$a) {
+                Some(o)
+            } else {
+                None
+            }
         }
     };
 }
 
-macro_rules! zero {
-    () => {
-        ///Set zero point on rectangular coordinates.
-        pub fn set_zero(&mut self, x: isize, y: isize) {
-            self.zero.x = x;
-            self.zero.y = y;
+macro_rules! attribute_get_or_insert {
+    ($n:ident, $t:ty, $a:tt, $v:expr) => {
+        pub(crate) fn $n(&mut self) -> Option<&mut $t> {
+            let a = AttrName::$a;
+            if !self.0.contains_key(&a) {
+                self.0.insert(a.clone(), Attribute::$a($v));
+            }
+            if let Some(Attribute::$a(o)) = self.0.get_mut(&a) {
+                Some(o)
+            } else {
+                None
+            }
         }
+    };
+}
 
-        pub fn set_zero_xyz(&mut self, x: isize, y: isize, z: isize) {
-            self.zero.x = x;
-            self.zero.y = y;
-            self.zero.z = z;
+macro_rules! resize {
+    () => {
+        pub(crate) fn resize(&mut self, c: &mut LayoutCoord) {
+            if let Ok(e) = self.element.read() {
+                self.rect.get_attr(&e, c)
+            }
         }
     };
 }
 
 macro_rules! right_bottom {
     () => {
-        pub(crate) fn right_bottom(&self) -> Coord2D {
-            self.outside.final_position().clone()
+        pub(crate) fn right_bottom(&self) -> Option<Coord2D> {
+            if self.rect.is_empty() {
+                return None;
+            }
+            if let Ok(e) = self.element().read() {
+                if let Some(a) = e.attribute().disabled() {
+                    if *a {
+                        return None;
+                    }
+                }
+            }
+            Some(self.rect.right_bottom())
         }
     };
 }
 
-macro_rules! range {
-    ($c:expr) => {{
-        let mut r = Range::new();
-        r.color = $c;
-        r
-    }};
-    ($c:expr, $x_rad:expr, $y_rad:expr) => {{
-        let mut r = Range::new();
-        r.color = $c;
-        r.x_rad = $x_rad;
-        r.y_rad = $y_rad;
-        r
-    }};
+macro_rules! draw_check {
+    ($self:ident) => {
+        if $self.rect.is_empty() {
+            return;
+        }
+        if let Ok(e) = $self.element().read() {
+            if let Some(a) = e.attribute().hidden() {
+                if *a {
+                    return;
+                }
+            }
+
+            if let Some(a) = e.attribute().disabled() {
+                if *a {
+                    return;
+                }
+            }
+        }
+    };
+}
+
+macro_rules! deref {
+    ($t:ty, $target:ty, $o:tt) => {
+        impl std::ops::Deref for $t {
+            type Target = $target;
+
+            fn deref(&self) -> &Self::Target {
+                &self.$o
+            }
+        }
+
+        impl std::ops::DerefMut for $t {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.$o
+            }
+        }
+    };
 }
