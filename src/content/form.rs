4d6befd35939b7702da4a50b3fee9ab4bf575093
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-12-01 16:15:03 +0800

    Update form.rs

--- src/content/form.rs
+++ src/content/form.rs
@@ -1,280 +1,412 @@
 use super::*;
-use crate::global::*;
-use crate::grid::{AlignPattern, ApplyFont};
-use crate::markup::{Mark, Page, VisionActionResult};
-use crate::parts::{Chronograph, Coord2D};
-use skia_safe::Canvas;
 use std::sync::{Arc, RwLock};
 
 ///"Button" represents a button.
-#[derive(Debug)]
+#[derive(Getters, MutGetters)]
 pub(crate) struct Button {
-    background: Box<dyn Painter>,
-    align_pattern: AlignPattern,
-    apply_font: ApplyFont,
+    #[getset(get = "pub(crate)")]
+    element: Arc<RwLock<Element>>,
+    #[getset(get = "pub(crate)", get_mut = "pub(crate)")]
+    rect: FixedRect,
+    painter: AppearanceComposite,
+    draw_text: DrawText,
     time_meter: Chronograph,
+    f: bool,
+}
+
+impl std::fmt::Debug for Button {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut f = f.debug_struct("Button");
+        if let Ok(o) = self.element.try_read() {
+            f.field("element", &o.to_string());
+        }
+        f.field("rect", &self.rect)
+            .field("painter", &self.painter)
+            .finish()
+    }
 }
 
 impl Button {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(element: Arc<RwLock<Element>>) -> Self {
         Self {
-            background: Box::new(range!(*default_surface_color(), 10, 10)),
-            align_pattern: AlignPattern::center_middle(),
-            apply_font: ApplyFont::new(),
+            element,
+            rect: FixedRect::with_side(100.0, 60.0),
+            painter: RoundRectangle {
+                color: *default_button_color(),
+                ..Default::default()
+            }
+            .into(),
+            draw_text: DrawText::new(AlignPattern::center_middle()),
             time_meter: Chronograph::new(1000),
+            f: false,
         }
     }
 
-    pub(crate) fn draw(&mut self, canvas: &Canvas, page: &mut Page, wrapper: &mut DrawUnitWrapper) {
-        let r = wrapper.rect();
+    resize!();
 
-        if self.time_meter.elapsed() {
-            self.background.as_mut().set_color(*default_surface_color());
+    right_bottom!();
+
+    pub(crate) fn draw(&mut self, t: &mut DrawCtx) {
+        draw_check!(self);
+
+        if self.f && self.time_meter.elapsed() {
+            self.painter = RoundRectangle {
+                color: *default_button_color(),
+                ..Default::default()
+            }
+            .into();
         }
-        if wrapper.cursor {
-            if let Some(a) = page.cursor.analyse() {
-                match a.1 {
-                    VisionActionResult::Press(_) => {
-                        self.background.as_mut().set_color(*default_button_color());
-                        self.time_meter.refresh();
+
+        self.painter.draw(&self.rect, t);
+
+        if let Ok(e) = self.element.read() {
+            self.draw_text.draw(&self.rect, e.text(), t);
+        }
+    }
+
+    pub(crate) fn consume_action(&mut self, t: &mut ActionCtx) {
+        match &t.kind {
+            ActionKind::Click(c, _) => {
+                if self.painter.within(&self.rect, c) {
+                    self.f = true;
+                    self.painter = RoundRectangle {
+                        color: *default_button2_color(),
+                        ..Default::default()
                     }
-                    _ => {}
+                    .into();
+                    self.time_meter.refresh();
+                    t.finish = true;
+                    return;
                 }
             }
-        }
-
-        self.background.as_mut().act(&r, canvas);
-
-        if let Ok(e) = wrapper.element.read() {
-            self.apply_font
-                .draw(&r, &self.align_pattern, &e.text, canvas);
+            _ => {}
         }
     }
-}
 
-///"Form" represents form.
-#[derive(Debug)]
-pub(crate) struct Form {}
-
-impl Form {
-    pub(crate) fn new() -> Self {
-        Self {}
-    }
-
-    fn fit(&mut self, mark_type: &Mark) -> bool {
-        match mark_type {
-            Mark::INP | Mark::PT | Mark::SELECT | Mark::TIME => true,
-            _ => false,
+    fn callback_action(&mut self) -> impl FnMut(&ActionKind, &mut PageContext) {
+        let o = self as *mut Self;
+        move |a, context| match &a {
+            _ => {}
         }
     }
 }
 
 ///"Inp" represents input.
+#[derive(Getters, MutGetters)]
 pub(crate) struct Inp {
-    input: Arc<RwLock<String>>,
-    background: Box<dyn Painter>,
-    align_pattern: AlignPattern,
-    apply_font: ApplyFont,
-    outside: Box<dyn OutPainter>,
-    scroll_bar: ScrollBar,
+    #[getset(get = "pub(crate)")]
+    element: Arc<RwLock<Element>>,
+    #[getset(get = "pub(crate)", get_mut = "pub(crate)")]
+    rect: FixedRect,
+    painter: AppearanceComposite,
+    draw_text: DrawText,
 }
 
 impl std::fmt::Debug for Inp {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut f = f.debug_struct("Inp");
-        if let Ok(o) = self.input.try_read() {
-            f.field("input", &o);
+        if let Ok(o) = self.element.try_read() {
+            f.field("element", &o.to_string());
         }
-        f.field("background", &self.background)
-            .field("align_pattern", &self.align_pattern)
-            .field("apply_font", &self.apply_font)
-            .field("outside", &self.outside)
-            .field("scroll_bar", &self.scroll_bar)
+        f.field("rect", &self.rect)
+            .field("painter", &self.painter)
             .finish()
     }
 }
 
 impl Inp {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(element: Arc<RwLock<Element>>) -> Self {
         Self {
-            input: Arc::new(RwLock::new(String::new())),
-            background: Box::new(Range::new()),
-            align_pattern: AlignPattern::left_middle(),
-            apply_font: ApplyFont::new(),
-            outside: Box::new(Border::new()),
-            scroll_bar: ScrollBar::new(),
+            element,
+            rect: FixedRect::with_side(100.0, 30.0),
+            painter: RectangleCurve {
+                color: *default_border_color(),
+                ..Default::default()
+            }
+            .into(),
+            draw_text: Default::default(),
         }
     }
 
+    resize!();
+
     right_bottom!();
 
-    pub(crate) fn draw(&mut self, canvas: &Canvas, page: &mut Page, wrapper: &mut DrawUnitWrapper) {
-        let r = wrapper.rect();
+    pub(crate) fn draw(&mut self, t: &mut DrawCtx) {
+        draw_check!(self);
 
-        self.outside.as_mut().act(&r, canvas);
-        self.background.as_mut().act(&r, canvas);
+        self.painter.draw(&self.rect, t);
 
-        if let Some(a) = page.cursor.analyse() {
-            match a.1 {
-                VisionActionResult::Press(_) => {
-                    if wrapper.cursor {
-                        self.apply_font.set_cursor(true);
-                        page.keyboard_input.replace(self.input.clone());
-                    } else {
-                        self.apply_font.set_cursor(false);
-                        page.keyboard_input.take();
-                    }
-                }
-                _ => {}
-            }
+        if let Ok(e) = self.element.read() {
+            self.draw_text.draw(&self.rect, e.get_value_or_text(), t);
         }
+    }
 
-        if let Ok(mut i) = self.input.try_write() {
-            if i.len() > 0 {
-                if let Ok(mut e) = wrapper.element.try_write() {
-                    e.text.push_str(i.drain(..).as_str());
+    pub(crate) fn consume_action(&mut self, t: &mut ActionCtx) {
+        match &t.kind {
+            ActionKind::Click(c, _) | ActionKind::DoubleClick(c, _) => {
+                if self.painter.within(&self.rect, c) {
+                    t.context.set_input_to(self.element.clone());
+                    self.draw_text.set_cursor(true);
+                    t.finish = true;
+                    t.callback.push(Box::new(self.callback_action()));
+                    return;
+                } else {
+                    t.context.take_input_to();
+                    self.draw_text.set_cursor(false);
                 }
             }
+            ActionKind::Focused(o) => {
+                if *o {
+                } else {
+                    self.draw_text.set_cursor(false);
+                }
+            }
+            _ => {}
         }
-        if let Ok(e) = wrapper.element.read() {
-            self.apply_font
-                .draw(&r, &self.align_pattern, &e.text, canvas);
+    }
+
+    fn callback_action(&mut self) -> impl FnMut(&ActionKind, &mut PageContext) {
+        let o = self as *mut Self;
+        move |a, context| match &a {
+            _ => {}
         }
     }
 }
 
 ///"Opt" represents an option.
-#[derive(Debug)]
+#[derive(Getters, MutGetters)]
 pub(crate) struct Opt {
-    background: Box<dyn Painter>,
-    align_pattern: AlignPattern,
-    apply_font: ApplyFont,
-    outside: Box<dyn OutPainter>,
+    #[getset(get = "pub(crate)")]
+    element: Arc<RwLock<Element>>,
+    #[getset(get = "pub(crate)", get_mut = "pub(crate)")]
+    rect: FixedRect,
+    painter: AppearanceComposite,
+}
+
+impl std::fmt::Debug for Opt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut f = f.debug_struct("Opt");
+        if let Ok(o) = self.element.try_read() {
+            f.field("element", &o.to_string());
+        }
+        f.field("rect", &self.rect)
+            .field("painter", &self.painter)
+            .finish()
+    }
 }
 
 impl Opt {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(element: Arc<RwLock<Element>>) -> Self {
         Self {
-            background: Box::new(Range::new()),
-            align_pattern: AlignPattern::left_middle(),
-            apply_font: ApplyFont::new(),
-            outside: Box::new(Border::new()),
+            element,
+            rect: FixedRect::with_side(100.0, 100.0),
+            painter: Default::default(),
         }
     }
 
-    pub(crate) fn draw(&mut self, canvas: &Canvas, page: &mut Page, wrapper: &mut DrawUnitWrapper) {
-        let r = wrapper.rect();
-
-        self.outside.as_mut().act(&r, canvas);
-        self.background.as_mut().act(&r, canvas);
-
-        if let Ok(e) = wrapper.element.read() {
-            self.apply_font
-                .draw(&r, &self.align_pattern, &e.text, canvas);
-        }
+    pub(crate) fn draw(&mut self, t: &mut DrawCtx) {
+        self.painter.draw(&self.rect, t);
     }
 }
 
 ///"Pt" represents plain text.
-#[derive(Debug)]
+#[derive(Getters, MutGetters)]
 pub(crate) struct Pt {
-    background: Box<dyn Painter>,
-    align_pattern: AlignPattern,
-    apply_font: ApplyFont,
-    outside: Box<dyn OutPainter>,
+    #[getset(get = "pub(crate)")]
+    element: Arc<RwLock<Element>>,
+    #[getset(get = "pub(crate)", get_mut = "pub(crate)")]
+    rect: FixedRect,
+    painter: AppearanceComposite,
+    draw_text: DrawText,
+    scroll_bar: ScrollBar,
+}
+
+impl std::fmt::Debug for Pt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut f = f.debug_struct("Pt");
+        if let Ok(o) = self.element.try_read() {
+            f.field("element", &o.to_string());
+        }
+        f.field("rect", &self.rect)
+            .field("painter", &self.painter)
+            .finish()
+    }
 }
 
 impl Pt {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(element: Arc<RwLock<Element>>) -> Self {
         Self {
-            background: Box::new(Range::new()),
-            align_pattern: AlignPattern::left_middle(),
-            apply_font: ApplyFont::new(),
-            outside: Box::new(Border::new()),
+            element,
+            rect: FixedRect::with_side(100.0, 100.0),
+            painter: RectangleCurve {
+                color: *default_border_color(),
+                ..Default::default()
+            }
+            .into(),
+            draw_text: Default::default(),
+            scroll_bar: Default::default(),
         }
     }
 
+    resize!();
+
     right_bottom!();
 
-    pub(crate) fn draw(&mut self, canvas: &Canvas, page: &mut Page, wrapper: &mut DrawUnitWrapper) {
-        let r = wrapper.rect();
+    pub(crate) fn draw(&mut self, t: &mut DrawCtx) {
+        draw_check!(self);
 
-        self.outside.as_mut().act(&r, canvas);
-        self.background.as_mut().act(&r, canvas);
+        self.painter.draw(&self.rect, t);
 
-        if let Ok(e) = wrapper.element.read() {
-            self.apply_font
-                .draw(&r, &self.align_pattern, &e.text, canvas);
+        if let Ok(e) = self.element.read() {
+            self.draw_text.draw(&self.rect, e.text(), t);
+        }
+    }
+
+    pub(crate) fn consume_action(&mut self, t: &mut ActionCtx) {
+        match &t.kind {
+            _ => {}
+        }
+    }
+
+    fn callback_action(&mut self) -> impl FnMut(&ActionKind, &mut PageContext) {
+        let o = self as *mut Self;
+        move |a, context| match &a {
+            _ => {}
         }
     }
 }
 
 ///"Select" represents a select.
-#[derive(Debug)]
+#[derive(Getters, MutGetters)]
 pub(crate) struct Select {
-    background: Box<dyn Painter>,
-    align_pattern: AlignPattern,
-    apply_font: ApplyFont,
-    outside: Box<dyn OutPainter>,
+    #[getset(get = "pub(crate)")]
+    element: Arc<RwLock<Element>>,
+    #[getset(get = "pub(crate)", get_mut = "pub(crate)")]
+    rect: FixedRect,
+    painter: AppearanceComposite,
+    draw_text: DrawText,
     scroll_bar: ScrollBar,
+    ops: Vec<Opt>,
+}
+
+impl std::fmt::Debug for Select {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut f = f.debug_struct("Select");
+        if let Ok(o) = self.element.try_read() {
+            f.field("element", &o.to_string());
+        }
+        f.field("rect", &self.rect)
+            .field("painter", &self.painter)
+            .finish()
+    }
 }
 
 impl Select {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(element: Arc<RwLock<Element>>) -> Self {
         Self {
-            background: Box::new(Range::new()),
-            align_pattern: AlignPattern::left_middle(),
-            apply_font: ApplyFont::new(),
-            outside: Box::new(Border::new()),
-            scroll_bar: ScrollBar::new(),
+            element,
+            rect: FixedRect::with_side(100.0, 100.0),
+            painter: RectangleCurve {
+                color: *default_border_color(),
+                ..Default::default()
+            }
+            .into(),
+            draw_text: Default::default(),
+            scroll_bar: Default::default(),
+            ops: Default::default(),
         }
     }
 
+    resize!();
+
     right_bottom!();
 
-    pub(crate) fn draw(&mut self, canvas: &Canvas, page: &mut Page, wrapper: &mut DrawUnitWrapper) {
-        let r = wrapper.rect();
+    pub(crate) fn draw(&mut self, t: &mut DrawCtx) {
+        draw_check!(self);
 
-        self.outside.as_mut().act(&r, canvas);
-        self.background.as_mut().act(&r, canvas);
+        self.painter.draw(&self.rect, t);
 
-        if let Ok(e) = wrapper.element.read() {
-            self.apply_font
-                .draw(&r, &self.align_pattern, &e.text, canvas);
+        if let Ok(e) = self.element.read() {
+            self.draw_text.draw(&self.rect, e.text(), t);
+        }
+    }
+
+    pub(crate) fn consume_action(&mut self, t: &mut ActionCtx) {
+        match t.kind {
+            _ => {}
+        }
+    }
+
+    fn callback_action(&mut self) -> impl FnMut(&ActionKind, &mut PageContext) {
+        let o = self as *mut Self;
+        move |a, context| match &a {
+            _ => {}
         }
     }
 }
 
 ///"Time" represents date time.
-#[derive(Debug)]
+#[derive(Getters, MutGetters)]
 pub(crate) struct Time {
-    background: Box<dyn Painter>,
-    align_pattern: AlignPattern,
-    apply_font: ApplyFont,
-    outside: Box<dyn OutPainter>,
+    #[getset(get = "pub(crate)")]
+    element: Arc<RwLock<Element>>,
+    #[getset(get = "pub(crate)", get_mut = "pub(crate)")]
+    rect: FixedRect,
+    painter: AppearanceComposite,
+    draw_text: DrawText,
+}
+
+impl std::fmt::Debug for Time {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut f = f.debug_struct("Time");
+        if let Ok(o) = self.element.try_read() {
+            f.field("element", &o.to_string());
+        }
+        f.field("rect", &self.rect)
+            .field("painter", &self.painter)
+            .finish()
+    }
 }
 
 impl Time {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(element: Arc<RwLock<Element>>) -> Self {
         Self {
-            background: Box::new(Range::new()),
-            align_pattern: AlignPattern::left_middle(),
-            apply_font: ApplyFont::new(),
-            outside: Box::new(Border::new()),
+            element,
+            rect: FixedRect::with_side(100.0, 100.0),
+            painter: Rectangle {
+                color: *default_surface_color(),
+                ..Default::default()
+            }
+            .into(),
+            draw_text: Default::default(),
         }
     }
 
+    resize!();
+
     right_bottom!();
 
-    pub(crate) fn draw(&mut self, canvas: &Canvas, page: &mut Page, wrapper: &mut DrawUnitWrapper) {
-        let r = wrapper.rect();
+    pub(crate) fn draw(&mut self, t: &mut DrawCtx) {
+        draw_check!(self);
 
-        self.outside.as_mut().act(&r, canvas);
-        self.background.as_mut().act(&r, canvas);
+        self.painter.draw(&self.rect, t);
 
-        if let Ok(e) = wrapper.element.read() {
-            self.apply_font
-                .draw(&r, &self.align_pattern, &e.text, canvas);
+        if let Ok(e) = self.element.read() {
+            self.draw_text.draw(&self.rect, e.text(), t);
+        }
+    }
+
+    pub(crate) fn consume_action(&mut self, t: &mut ActionCtx) {
+        match t.kind {
+            _ => {}
+        }
+    }
+
+    fn callback_action(&mut self) -> impl FnMut(&ActionKind, &mut PageContext) {
+        let o = self as *mut Self;
+        move |a, context| match &a {
+            _ => {}
         }
     }
 }
