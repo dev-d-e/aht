b58fca8ea7cf1f106aa314e1d5bacf1abd5577de
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-12-01 16:23:18 +0800

    Create mod.rs

---
+++ src/gpu/shader/mod.rs
@@ -0,0 +1,41 @@
+use super::*;
+use getset::Getters;
+use pollster::block_on;
+use wgpu::{
+    include_wgsl, Device, DeviceDescriptor, DownlevelFlags, Features, Instance, InstanceDescriptor,
+    Limits, MemoryHints, Queue, RequestAdapterOptions, Trace,
+};
+
+#[derive(Getters)]
+pub struct ComputeQueueHolder {
+    #[getset(get = "pub")]
+    device: Device,
+    #[getset(get = "pub")]
+    queue: Queue,
+}
+
+impl ComputeQueueHolder {
+    pub fn new() -> Result<Self> {
+        let instance = Instance::new(&InstanceDescriptor::default());
+
+        let adapter = block_on(instance.request_adapter(&RequestAdapterOptions::default()))
+            .map_err(|e| to_err(e))?;
+
+        let o = adapter.get_downlevel_capabilities();
+        if !o.flags.contains(DownlevelFlags::COMPUTE_SHADERS) {
+            return Err(ErrorKind::GpuError.into());
+        }
+
+        let device_descriptor = DeviceDescriptor {
+            label: None,
+            required_features: Features::empty(),
+            required_limits: Limits::downlevel_defaults(),
+            memory_hints: MemoryHints::MemoryUsage,
+            trace: Trace::Off,
+            ..Default::default()
+        };
+        block_on(adapter.request_device(&device_descriptor))
+            .map_err(|e| to_err(e))
+            .map(|(device, queue)| Self { device, queue })
+    }
+}
