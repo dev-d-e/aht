e92309b4e1a22c5fa49b76a8a34a3f436c29e021
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-12-01 16:49:49 +0800

    Create mod.rs

---
+++ src/screen/mod.rs
@@ -0,0 +1,52 @@
+#[cfg(feature = "window")]
+mod window;
+
+#[cfg(feature = "window")]
+pub use self::window::*;
+use crate::error::*;
+use crate::markup::*;
+use crate::page::*;
+use crate::utils::*;
+use getset::{Getters, MutGetters};
+use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
+use std::any::Any;
+use std::sync::Arc;
+
+#[inline(always)]
+fn to_err(e: impl std::error::Error + 'static) -> Error {
+    (ErrorKind::GpuError, e).into()
+}
+
+///Represents renderer.
+#[derive(Getters, MutGetters)]
+pub struct Renderer {
+    #[cfg(feature = "vulkan")]
+    r: crate::gpu::vk::VkRenderer,
+}
+
+impl Renderer {
+    pub fn new(
+        w: Arc<impl HasWindowHandle + HasDisplayHandle + Any + Send + Sync>,
+    ) -> Result<Self> {
+        if cfg!(feature = "vulkan") {
+            crate::gpu::vk::SurfaceQueueHolder::new(w)?
+                .try_into()
+                .map(|r| Self { r })
+        } else {
+            warn!("There is no renderer");
+            Err(ErrorKind::None.into())
+        }
+    }
+
+    ///Resize physical width and height.
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.r.resize(width, height);
+    }
+
+    ///Render a page.
+    pub fn draw(&mut self, page: &mut Page) {
+        self.r.draw(|s| {
+            page.draw_body(s);
+        });
+    }
+}
