31859c10fa0efa741f8061197ad2ceba259d2873
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-12-01 16:22:40 +0800

    Update mod.rs

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -1,2 +1,11 @@
+pub mod shader;
 #[cfg(feature = "vulkan")]
 pub mod vk;
+
+use crate::error::*;
+use crate::utils::*;
+
+#[inline(always)]
+fn to_err(e: impl std::error::Error + 'static) -> Error {
+    (ErrorKind::GpuError, e).into()
+}
