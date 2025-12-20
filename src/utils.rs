75a5abd3b84f7751c4daf720f48a496fc024e33f
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-12-20 17:18:16 +0800

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -4,66 +4,132 @@ use std::str::FromStr;
 use std::time::{Duration, Instant};
 
 pub(crate) mod ascii {
-    //'<'
-    pub(crate) const LT: char = '<';
-    //'>'
-    pub(crate) const GT: char = '>';
-    //'/'
-    pub(crate) const SLASH: char = '/';
-    //' '
+    ///' '
     pub(crate) const SPACE: char = ' ';
-    //'\r'
-    pub(crate) const CR: char = '\r';
-    //'\n'
-    pub(crate) const LF: char = '\n';
-    //'='
-    pub(crate) const EQUAL: char = '=';
-    //'"'
+    ///!
+    pub(crate) const EXCLAMATION: char = '!';
+    ///"
     pub(crate) const QUOTATION: char = '"';
-    //'''
-    pub(crate) const APOSTROPHE: char = '\'';
-    //','
-    pub(crate) const COMMA: char = ',';
-    //'['
-    pub(crate) const LEFT_SQUARE_BRACKET: char = '[';
-    //']'
-    pub(crate) const RIGHT_SQUARE_BRACKET: char = ']';
-    //'%'
+    ///#
+    pub(crate) const NUMBER_SIGN: char = '#';
+    ///%
     pub(crate) const PER_CENT: char = '%';
-    //'&'
+    ///&
     pub(crate) const AMPERSAND: char = '&';
-    //';'
-    pub(crate) const SEMICOLON: char = ';';
-    //'@'
-    pub(crate) const AT: char = '@';
-    //'.'
+    ///'
+    pub(crate) const APOSTROPHE: char = '\'';
+    ///(
+    pub(crate) const LEFT_PARENTHESIS: char = '(';
+    ///)
+    pub(crate) const RIGHT_PARENTHESIS: char = ')';
+    ///*
+    pub(crate) const ASTERISK: char = '*';
+    ///+
+    pub(crate) const PLUS: char = '+';
+    ///,
+    pub(crate) const COMMA: char = ',';
+    ///-
+    pub(crate) const HYPHEN: char = '-';
+    ///.
     pub(crate) const FULL_STOP: char = '.';
-    //':'
+    ///\/
+    pub(crate) const SLASH: char = '/';
+    ///\n
+    pub(crate) const LF: char = '\n';
+    ///\r
+    pub(crate) const CR: char = '\r';
+    ///:
     pub(crate) const COLON: char = ':';
-    //'{'
+    ///;
+    pub(crate) const SEMICOLON: char = ';';
+    ///<
+    pub(crate) const LT: char = '<';
+    ///>
+    pub(crate) const GT: char = '>';
+    ///=
+    pub(crate) const EQUAL: char = '=';
+    ///?
+    pub(crate) const QUESTION: char = '?';
+    ///@
+    pub(crate) const AT: char = '@';
+    ///[
+    pub(crate) const LEFT_SQUARE_BRACKET: char = '[';
+    ///]
+    pub(crate) const RIGHT_SQUARE_BRACKET: char = ']';
+    ///\
+    pub(crate) const BACKSLASH: char = '\\';
+    ///^
+    pub(crate) const CIRCUMFLEX_ACCENT: char = '^';
+    ///_
+    pub(crate) const LOW_LINE: char = '_';
+    ///{
     pub(crate) const LEFT_CURLY_BRACKET: char = '{';
-    //'}'
+    ///}
     pub(crate) const RIGHT_CURLY_BRACKET: char = '}';
+    ///|
+    pub(crate) const VERTICAL_LINE: char = '|';
+
+    use crate::error::*;
+
+    #[derive(Debug, Default)]
+    pub(crate) struct CharCounter {
+        total: usize,
+        row: usize,
+        column: usize,
+        cr: bool,
+    }
+
+    impl CharCounter {
+        pub(crate) fn count(&mut self, c: char) {
+            self.total += 1;
+            match c {
+                CR => {
+                    self.row += 1;
+                    self.column = 0;
+                    self.cr = true;
+                }
+                LF => {
+                    if !self.cr {
+                        self.row += 1;
+                        self.column = 0;
+                    }
+                    self.cr = false;
+                }
+                _ => {
+                    self.column += 1;
+                    self.cr = false;
+                }
+            }
+        }
+
+        pub(crate) fn to_error(&self, k: ErrorKind, s: impl ToString) -> Error {
+            (k, self.row, self.column, s).into()
+        }
+    }
 }
 
 #[inline(always)]
 pub(crate) fn to_bool(s: &str) -> Result<bool> {
-    bool::from_str(s).map_err(|e| e.into())
+    if s.is_empty() {
+        Ok(true)
+    } else {
+        bool::from_str(s).map_err(|e| (ErrorKind::StrErr, e).into())
+    }
 }
 
 #[inline(always)]
 pub(crate) fn to_usize(s: &str) -> Result<usize> {
-    usize::from_str_radix(s, 10).map_err(|e| e.into())
+    usize::from_str_radix(s, 10).map_err(|e| (ErrorKind::StrErr, e).into())
 }
 
 #[inline(always)]
 pub(crate) fn to_isize(s: &str) -> Result<isize> {
-    isize::from_str_radix(s, 10).map_err(|e| e.into())
+    isize::from_str_radix(s, 10).map_err(|e| (ErrorKind::StrErr, e).into())
 }
 
 #[inline(always)]
 pub(crate) fn to_f32(s: &str) -> Result<f32> {
-    f32::from_str(s).map_err(|e| e.into())
+    f32::from_str(s).map_err(|e| (ErrorKind::StrErr, e).into())
 }
 
 #[inline(always)]
