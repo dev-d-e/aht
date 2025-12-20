9a681ac3d067e663101c006d6e0bcb032292892b
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2025-12-20 17:19:41 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,34 +7,64 @@ The components build on plane rectangular coordinates.
 ```
 use aht::Page;
 
-aht::markup::Element::parse_one("<aht>
-    <head>
-        <title></title>
-    </head>
-    <body column='[100,100]' row='[100,100],2'>
-        <inp name='' value='' readonly required>input</inp>
-        <button href=''>button</button>
-        <area class='' id='' width='1000' height='100' column='2' row=''></area>
-    </body>
-    <style>
-    </style>
-    <script>
-    </script>
- </aht>");
 Page::parse("<aht>
     <head>
         <title></title>
     </head>
-    <body column='[100,100]' row='[100,100],2'>
-        <inp name='' value='' readonly required>input</inp>
-        <button href=''>button</button>
-        <area class='' id='' width='1000' height='100' column='2' row=''></area>
+    <body>
+        <inp name="" value="" readonly required>input</inp>
+        <button href="" class=a>button</button>
+        <area id="b"></area>
     </body>
     <style>
+    body {
+        column:[100,100];
+        row:[100,100],2;
+    }
+    inp {
+        ordinal:0;
+        height:100;
+    }
+    .a {
+        position=100,100;
+    }
+    .id = b {
+        width:1000;
+        height:100;
+        column:2;
+        row:1;
+    }
     </style>
     <script>
     </script>
  </aht>");
+
+ Page::parse_s("<aht>
+    < head>
+        < title>
+    <:body>
+        < inp name="" value="" readonly required>input
+        <button href="" class=a>button
+        <area class="" id="b">
+    <:style>
+    body {
+        column:[100,100];
+        row:[100,100],2;
+    }
+    inp {
+        ordinal:0;
+        height:100;
+    }
+    .a {
+        position=100,100;
+    }
+    .id = b {
+        width:1000;
+        height:100;
+        column:2;
+        row:1;
+    }
+    <script>");
 ```
 
 # Cargo Features
