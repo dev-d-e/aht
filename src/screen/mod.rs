519d7963de5ad4117f145f16af293b10c76504a1
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2026-05-24 16:32:36 +0800

    Update mod.rs

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -7,7 +7,6 @@ use crate::error::*;
 use crate::markup::*;
 use crate::page::*;
 use crate::utils::*;
-use getset::{Getters, MutGetters};
 use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 use std::any::Any;
 use std::sync::Arc;
@@ -15,22 +14,28 @@ use std::sync::Arc;
 ///Represents renderer.
 #[derive(Getters, MutGetters)]
 pub struct Renderer {
-    #[cfg(feature = "vulkan")]
+    #[cfg(any(target_os = "linux", target_os = "android"))]
     r: crate::gpu::vk::VkRenderer,
 }
 
+#[cfg(any(
+    target_os = "linux",
+    target_os = "android",
+    target_os = "ios",
+    target_os = "macos",
+    target_os = "windows"
+))]
 impl Renderer {
-    pub fn new(
-        w: Arc<impl HasWindowHandle + HasDisplayHandle + Any + Send + Sync>,
-    ) -> Result<Self> {
-        if cfg!(feature = "vulkan") {
-            crate::gpu::vk::SurfaceQueueHolder::new(w)?
-                .try_into()
-                .map(|r| Self { r })
-        } else {
-            warn!("There is no renderer");
-            Err((ErrorKind::Gpu, "no renderer").into())
-        }
+    #[cfg(any(target_os = "linux", target_os = "android"))]
+    pub fn new<T>(w: Arc<T>) -> Option<Self>
+    where
+        T: HasWindowHandle + HasDisplayHandle + Any + Send + Sync,
+    {
+        crate::gpu::vk::SurfaceQueueHolder::new(w)
+            .and_then(|o| o.try_into())
+            .map_err(|e| error!("Renderer: {e}"))
+            .map(|r| Self { r })
+            .ok()
     }
 
     ///Resize physical width and height.
@@ -44,4 +49,25 @@ impl Renderer {
             page.draw_body(s);
         });
     }
+}
+
+#[cfg(not(any(
+    target_os = "linux",
+    target_os = "android",
+    target_os = "ios",
+    target_os = "macos",
+    target_os = "windows"
+)))]
+impl Renderer {
+    pub fn new<T>(w: Arc<T>) -> Option<Self>
+    where
+        T: HasWindowHandle + HasDisplayHandle + Any + Send + Sync,
+    {
+        error!("There is no renderer");
+        None
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {}
+
+    pub fn draw(&mut self, page: &mut Page) {}
 }
